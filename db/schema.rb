f7bf52423e384f0931d985351c86c60e2e1262af
Maria <mliberov@gmail.com>
2015-05-14 17:58:46 -0400

    Add company

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514021537) do
+ActiveRecord::Schema.define(version: 20150514215325) do
 
   create_table "jobs", force: :cascade do |t|
     t.text     "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150514021537) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "location"
+    t.string   "company"
   end
 
 end
