b3242f01fb936ffe12829a3c55b8dd1c0a9f2040
Maria <mliberov@gmail.com>
2015-05-13 22:17:32 -0400

    Add location

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314014635) do
+ActiveRecord::Schema.define(version: 20150514021537) do
 
   create_table "jobs", force: :cascade do |t|
     t.text     "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "location"
   end
 
 end
